ff2aed47b7d3be4a16aeb09292f3aee9ae0b7248
rogithub <rosec@protonmail.ch>
2015-04-13 08:42:51 -0500

    added pattern di container

--- patterns/di/myClass.js
+++ patterns/di/myClass.js
@@ -1,11 +1,25 @@
+
+/**
+ * Dummy class that receives an object cappable of writing to the console.
+ * @param {Object} dependency Dependency for this class (console object).
+ */
 function MyClass(dependency) {
     this.dependency = dependency;
 }
 
+/**
+ * Prints out a given text to the console.
+ * @param {String} text Any text.
+ */
 MyClass.prototype.print = function(text) {
     this.dependency.log(text);
 }
 
+/**
+ * Factory function.
+ * @param   {Object} dependency Dependency for this class (console objec)
+ * @returns {Object} An instance of MyClass
+ */
 module.exports = function (dependency) {
     return new MyClass(dependency);
 }
--- patterns/di/usage.js
+++ patterns/di/usage.js
@@ -1,8 +1,14 @@
+// load dependency container
 var diContainer = require('./diContainer')();
 
+//register parameter 'dependency'.
 diContainer.register('dependency', console);
+
+//register factory method.
 diContainer.factory('myClass', require('./myClass'));
 
+//obtain an instance of MyClass
 var myObject = diContainer.get('myClass');
 
+//calling a method on an object for which we never used 'new' directly.
 myObject.print('Hello dependency injection container');
