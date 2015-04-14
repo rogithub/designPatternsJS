90ba99bc2149a8061ea8ed544e920cf56d463120
rogithub <rosec@protonmail.ch>
2015-04-14 07:48:21 -0500

    added patterns

---
+++ scalability/horizontal/reverseProxy/app.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var pid = process.pid;
+
+http.createServer(function(req, res) {
+    for(var i = 1e7; i > 0; i--) {
+    
+    }
+
+    console.log('Handling request from the pid:' + pid);
+    res.end('Hello from the pid: ' + pid + '\n');
+
+}).listen(process.env.PORT || process.argv[2] || 8080, function() { 
+    console.log('Started ' + pid);
+});
