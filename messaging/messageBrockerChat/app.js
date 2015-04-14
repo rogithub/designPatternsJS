fb155a53b823a708a2c8278d1c281890505cd10c
rogithub <rosec@protonmail.ch>
2015-04-14 15:43:01 -0500

    messaging

--- messaging/messageBrockerChat/app.js
+++ messaging/messageBrockerChat/app.js
@@ -1,4 +1,7 @@
 var WebSocketServer = require('ws').Server;
+var redis = require("redis"); 
+var redisSub = redis.createClient();
+var redisPub = redis.createClient();
 
 //static file server
 var server = require('http').createServer(
@@ -11,14 +14,15 @@ wss.on('connection', function(ws) {
     console.log('Client connected');
     ws.on('message', function(msg) {
         console.log('Message: ' + msg);
-        broadcast(msg);
+        redisPub.publish('chat_messages', msg);
     });
 });
 
-function broadcast(msg) {
+redisSub.subscribe('chat_messages'); 
+redisSub.on('message', function(channel, msg) {
     wss.clients.forEach(function(client) {
         client.send(msg);
-    });
-}
+    }); 
+});
 
 server.listen(process.argv[2] || 8080);
